b4d0ecb9cc1f8a5b37c70824651ad8a39c4e8417
pho <pho@gatchan.org>
2015-07-19 13:56:46 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/BurntSushi/toml"
+)
+
+func main() {
+
+	configFile := flag.String("c", "config.toml", "Config file path")
+	flag.Parse()
+
+	findSocatBinary()
+
+	var config Services
+	var md toml.MetaData
+	var err error
+	if md, err = toml.DecodeFile(*configFile, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	services := make(map[string]*Service)
+
+	for name, serv := range config {
+		s := NewService()
+		s.Name = name
+
+		services[name] = s
+
+		for _, k := range md.Keys() {
+			ks := strings.Split(k.String(), ".")
+			if len(ks) > 1 && ks[0] == name {
+				switch ks[1] {
+				case "srcport":
+					s.Srcport = serv.Srcport
+				case "srcflags":
+					s.Srcflags = serv.Srcflags
+				case "srcproto":
+					s.Srcproto = serv.Srcproto
+				case "dsthost":
+					s.Dsthost = serv.Dsthost
+				case "dstproto":
+					s.Dstproto = serv.Dstproto
+				case "dstport":
+					s.Dstport = serv.Dstport
+				}
+			}
+		}
+	}
+
+	log.Println("Found", len(services), "services")
+
+	for _, s := range services {
+		s.Start()
+	}
+
+	for {
+		<-time.NewTimer(time.Hour).C
+	}
+}
+
+func findSocatBinary() {
+	c := exec.Command("which", "socat")
+	o, _ := c.Output()
+	err := c.Run()
+	if err != nil {
+		if _, err := os.Stat(*bin); os.IsNotExist(err) {
+			log.Fatalln("Couldnt find a socat binary")
+		}
+	} else {
+		*bin = string(o)
+	}
+}
---
+++ service.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
+	"time"
+)
+
+var bin = flag.String("bin", "/usr/bin/socat", "Socat binary path")
+
+const (
+	STOPPED = iota
+	RUNNING
+)
+
+type Services map[string]Service
+
+type Service struct {
+	Name  string
+	State int
+
+	Srcport  int      `toml:"srcport"`
+	Srcflags []string `toml:"srcflags"`
+	Srcproto string   `toml:"srcproto"`
+
+	Dsthost  string `toml:"dsthost"`
+	Dstport  int    `toml:"dstport"`
+	Dstproto string `toml:"dstproto"`
+
+	cmd     *exec.Cmd
+	cmdArgs []string
+	restart bool
+}
+
+func NewService() (s *Service) {
+	s = new(Service)
+
+	s.cmdArgs = make([]string, 2)
+	s.State = STOPPED
+	s.restart = false
+
+	s.Srcport = 80
+	s.Srcflags = []string{"fork", "reuseaddr"}
+	s.Srcproto = "TCP4-LISTEN"
+
+	s.Dsthost = "localhost"
+	s.Dstport = 8080
+	s.Dstproto = "TCP4"
+
+	return s
+}
+
+func (s *Service) Start() {
+	if s.State == RUNNING {
+		return
+	}
+
+	flags := ""
+	for _, f := range s.Srcflags {
+		flags = fmt.Sprintf("%s,%s", flags, f)
+	}
+
+	s.cmdArgs[0] = fmt.Sprintf("%s:%d%s",
+		s.Srcproto,
+		s.Srcport,
+		flags)
+
+	s.cmdArgs[1] = fmt.Sprintf("%s:%s:%d",
+		s.Dstproto,
+		s.Dsthost,
+		s.Dstport)
+
+	s.restart = true
+
+	go s.run()
+}
+
+func (s *Service) run() {
+	for s.restart {
+
+		s.cmd = exec.Command(*bin, s.cmdArgs...)
+
+		s.State = RUNNING
+		err := s.cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(s.Name, s.cmd.Args)
+		err = s.cmd.Wait()
+		log.Println(s.Name, err)
+		<-time.NewTimer(time.Second * 3).C
+		s.State = STOPPED
+	}
+}
+
+func (s *Service) Stop() {
+	s.restart = false
+	if s.State == RUNNING {
+		s.cmd.Process.Kill()
+	}
+	s.State = STOPPED
+}
